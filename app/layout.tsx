022cdf4bacf3cde394e0f62fd08edaec6841e9b6
kim <k0987139149@gmail.com>
2026-05-05 18:28:47 +0800

    feat: 建立多語言系統

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { CurrencyProvider } from "./CurrencyContext";
+import { CurrencyProvider } from './CurrencyContext'
+import { LanguageProvider } from './LanguageContext'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +30,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <CurrencyProvider>
-          {children}
-        </CurrencyProvider>
+        <LanguageProvider>
+  <CurrencyProvider>
+    {children}
+  </CurrencyProvider>
+</LanguageProvider>
       </body>
     </html>
   );
